32cceb9cd747828fed3a593b88747aefc0efa987
Pavel Atanasov <apavel@vmware.com>
2022-12-19 11:32:21 +0200

    feat(parser): make monoidal on error accumulation

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTSyntax #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Parser
   ( ParseError (..),
@@ -12,14 +14,6 @@ where
 
 import Control.Applicative (Alternative (..))
 
--- newtype Parser' i e a = Parser'
---   { runParser' :: [i] -> Either [ParseError i e] ([i], a)
---   }
---   deriving stock (Generic1)
---   deriving
---     (Functor, Applicative)
---     via ((,) [i] `Compose` Either [ParseError i e] `Compose` (->) [i])
-
 data ParseError i e
   = EndOfInput
   | Unexpected i
@@ -27,49 +21,49 @@ data ParseError i e
   | Empty
   deriving (Eq, Show)
 
-newtype Parser i e a where
+newtype Parser i m e a where
   Parser ::
-    {runParser :: [i] -> Either [ParseError i e] (a, [i])} ->
-    Parser i e a
+    {runParser :: [i] -> Either (m (ParseError i e)) (a, [i])} ->
+    Parser i m e a
 
-instance Functor (Parser i e) where
-  fmap :: (a -> b) -> Parser i e a -> Parser i e b
+instance (Monoid (m (ParseError i e))) => Functor (Parser i m e) where
+  fmap :: (a -> b) -> Parser i m e a -> Parser i m e b
   fmap f (Parser p) = Parser $ \input -> do
     (output, rest) <- p input
     pure (f output, rest)
 
-instance Applicative (Parser i e) where
-  pure :: a -> Parser i e a
+instance (Monoid (m (ParseError i e))) => Applicative (Parser i m e) where
+  pure :: a -> Parser i m e a
   pure a = Parser $ \input -> Right (a, input)
 
-  (<*>) :: Parser i e (a -> b) -> Parser i e a -> Parser i e b
+  (<*>) :: Parser i m e (a -> b) -> Parser i m e a -> Parser i m e b
   Parser f <*> Parser p = Parser $ \input -> do
     (f', rest) <- f input
     (output, rest') <- p rest
     pure (f' output, rest')
 
-instance Monad (Parser i e) where
-  (>>=) :: Parser i e a -> (a -> Parser i e b) -> Parser i e b
+instance (Monoid (m (ParseError i e))) => Monad (Parser i m e) where
+  (>>=) :: Parser i m e a -> (a -> Parser i m e b) -> Parser i m e b
   (Parser p) >>= k = Parser $ \input -> do
     (output, rest) <- p input
     runParser (k output) rest
 
-instance Alternative (Parser i e) where
-  empty :: Parser i e a
-  empty = Parser $ const $ Left [Empty]
+instance (Monoid (m (ParseError i e)), Alternative m) => Alternative (Parser i m e) where
+  empty :: Parser i m e a
+  empty = Parser $ const $ Left $ pure Empty
 
-  (<|>) :: Parser i e a -> Parser i e a -> Parser i e a
+  (<|>) :: Parser i m e a -> Parser i m e a -> Parser i m e a
   (Parser l) <|> (Parser r) = Parser $ \input ->
     case l input of
       Left err_l ->
         case r input of
-          Left err_r -> Left $ err_l <> err_r
+          Left err_r -> Left $ err_l <|> err_r
           Right (output_r, rest_r) -> Right (output_r, rest_r)
       Right (output_l, rest_l) -> Right (output_l, rest_l)
 
 ---
 
-satisfy :: (i -> Bool) -> Parser i e i
+satisfy :: (i -> Bool) -> Parser i [] e i
 satisfy predicate = Parser $ \input ->
   case input of
     [] -> Left [EndOfInput]
@@ -77,8 +71,8 @@ satisfy predicate = Parser $ \input ->
       | predicate x -> Right (x, rest)
       | otherwise -> Left [Unexpected x]
 
-char :: Eq i => i -> Parser i e i
+char :: Eq i => i -> Parser i [] e i
 char i = satisfy (== i)
 
-string :: Eq i => [i] -> Parser i e [i]
+string :: Eq i => [i] -> Parser i [] e [i]
 string = traverse char
